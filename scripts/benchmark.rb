ece3744c4b61d8237723986ca8ffd4040c65036b
Ryo Nakamura <r7kamura@gmail.com>
2013-11-28 00:43:26 +0900

    Measure scale benchmark by changing routes size

--- scripts/benchmark.rb
+++ scripts/benchmark.rb
@@ -1,28 +1,25 @@
-# Benchmark script for comparison of the routing algorithm between v0.0.2 and v0.0.3.
-# In my laptop environment, v0.0.3 is 17x faster than 0.0.2 with 676 routes & 100,000 tries.
-
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "rack/multiplexer"
 require "benchmark"
 
-multiplexer = Rack::Multiplexer.new
-(?a..?z).each do |head|
-  (?a..?z).each do |tail|
-    multiplexer.get("/#{head}#{tail}") do
-      [200, {}, ["OK"]]
-    end
-  end
-end
-
+characters = (?a..?z).to_a
 env = {
   "PATH_INFO" => "/mm",
   "REQUEST_METHOD" => "GET",
 }
-
 n = 100_000
-puts Benchmark::CAPTION
-puts Benchmark.measure {
+
+26.times do |i|
+  multiplexer = Rack::Multiplexer.new
+  characters[0, i + 1].each do |character|
+    multiplexer.get("/#{character}") do
+      [200, {}, ["OK"]]
+    end
+  end
+
+  before = Time.now
   n.times do
     multiplexer.call(env)
   end
-}
+  puts Time.now - before
+end
